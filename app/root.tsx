d8c17518e410b4da8a8ef372d38ed8d7f02cda76
brookslybrand <brookslybrand@gmail.com>
2023-07-11 18:17:38 -0500

    Add simple header with avatar

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,7 @@
 import { cssBundleHref } from '@remix-run/css-bundle';
 import type { LinksFunction } from '@remix-run/node';
 import {
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -8,14 +9,110 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
+import { UserIcon } from '@heroicons/react/20/solid';
+import { clsx } from 'clsx';
 
 import styles from './tailwind.css';
+import { Menu, Transition } from '@headlessui/react';
+import { Fragment, useState } from 'react';
 
 export const links: LinksFunction = () => [
-   { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' },
+  { rel: 'stylesheet', href: 'https://rsms.me/inter/inter.css' },
   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
   { rel: 'stylesheet', href: styles },
 ];
+
+const avatarFocusCss =
+  'flex rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800';
+
+function Header() {
+  // TODO: hook up logic
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <header className="absolute inset-x-0 top-0 z-50 flex h-16 border-b border-gray-900/10">
+      <div className="mx-auto flex w-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-1 items-center justify-end gap-x-8">
+          <button onClick={() => setIsLoggedIn((p) => !p)}>Toggle</button>
+          {isLoggedIn ? (
+            <Menu as="div">
+              <Menu.Button className={clsx('bg-gray-800', avatarFocusCss)}>
+                <span className="sr-only">Open user menu</span>
+                <img
+                  className="h-8 w-8 rounded-full"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+              </Menu.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to="#"
+                        className={clsx(
+                          active ? 'bg-gray-100' : '',
+                          'block px-4 py-2 text-sm text-gray-700',
+                        )}
+                      >
+                        Your Profile
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to="#"
+                        className={clsx(
+                          active ? 'bg-gray-100' : '',
+                          'block px-4 py-2 text-sm text-gray-700',
+                        )}
+                      >
+                        Settings
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to="#"
+                        className={clsx(
+                          active ? 'bg-gray-100' : '',
+                          'block px-4 py-2 text-sm text-gray-700',
+                        )}
+                      >
+                        Sign out
+                      </Link>
+                    )}
+                  </Menu.Item>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          ) : (
+            <Link
+              to="login"
+              className={clsx(
+                'text-gray-600 ring-1 ring-gray-600/40 ',
+                avatarFocusCss,
+              )}
+            >
+              <span className="sr-only">Log in</span>
+              <UserIcon className="h-8 w-8 rounded-full" />
+            </Link>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+}
 
 export default function App() {
   return (
@@ -28,6 +125,7 @@ export default function App() {
       </head>
       <body>
         <Outlet />
+        <Header />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,26 +7,20 @@ export const meta: V2_MetaFunction = () => {
   ];
 };
 
-function Header() {
-  return (
-    <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="mx-auto max-w-2xl text-center">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-          The Lybrary
-        </h1>
-        <p className="mt-6 text-lg leading-8 text-gray-600">
-          The digital library of the Lybrand's ever expanding board game
-          collection
-        </p>
-      </div>
-    </div>
-  );
-}
-
 export default function Index() {
   return (
     <div>
-      <Header />
+      <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            The Lybrary
+          </h1>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            The digital library of the Lybrand's ever expanding board game
+            collection
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
