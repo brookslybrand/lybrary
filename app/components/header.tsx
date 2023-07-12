892cb72d373c85e363435af0d43ce5910d644037
brookslybrand <brookslybrand@gmail.com>
2023-07-12 09:15:07 -0500

    Cleanup header and add CTA links

---
+++ app/components/header.tsx
@@ -0,0 +1,98 @@
+import { Fragment, useState } from 'react';
+import { Link } from '@remix-run/react';
+import { UserIcon } from '@heroicons/react/20/solid';
+import { clsx } from 'clsx';
+
+import { Menu, Transition } from '@headlessui/react';
+
+const avatarFocusCss =
+  'flex rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800';
+
+export function Header() {
+  // TODO: hook up logic
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <header className="absolute inset-x-0 top-0 z-50 flex h-16 border-b border-slate-700/10 bg-gradient-to-tl from-teal-50/80 to-teal-300/50">
+      <div className="mx-auto flex w-full max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8">
+        <div className="flex flex-1 items-center justify-end gap-x-8">
+          <button onClick={() => setIsLoggedIn((p) => !p)}>Toggle</button>
+          {isLoggedIn ? (
+            <Menu as="div">
+              <Menu.Button className={clsx('bg-gray-800', avatarFocusCss)}>
+                <span className="sr-only">Open user menu</span>
+                <img
+                  className="h-8 w-8 rounded-full"
+                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+              </Menu.Button>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to="#"
+                        className={clsx(
+                          active ? 'bg-gray-100' : '',
+                          'block px-4 py-2 text-sm text-gray-700',
+                        )}
+                      >
+                        Your Profile
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to="#"
+                        className={clsx(
+                          active ? 'bg-gray-100' : '',
+                          'block px-4 py-2 text-sm text-gray-700',
+                        )}
+                      >
+                        Settings
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to="#"
+                        className={clsx(
+                          active ? 'bg-gray-100' : '',
+                          'block px-4 py-2 text-sm text-gray-700',
+                        )}
+                      >
+                        Sign out
+                      </Link>
+                    )}
+                  </Menu.Item>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          ) : (
+            <Link
+              to="login"
+              className={clsx(
+                'text-gray-600 ring-1 ring-gray-600/40 ',
+                avatarFocusCss,
+              )}
+            >
+              <span className="sr-only">Log in</span>
+              <UserIcon className="h-8 w-8 rounded-full" />
+            </Link>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { V2_MetaFunction } from '@remix-run/node';
+import { Link } from '@remix-run/react';
+import { clsx } from 'clsx';
 
 export const meta: V2_MetaFunction = () => {
   return [
@@ -9,18 +11,47 @@ export const meta: V2_MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div>
-      <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
-        <div className="mx-auto max-w-2xl text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            The Lybrary
-          </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            The digital library of the Lybrand's ever expanding board game
-            collection
-          </p>
+    <div className="px-6 py-24 sm:py-32 lg:px-8">
+      <div className="mx-auto flex max-w-md flex-col items-center text-center lg:max-w-fit">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          The Lybrary
+        </h1>
+        <p className="mt-6 text-base leading-8 text-gray-600 lg:mt-8 lg:text-lg">
+          The digital library of the Lybrand's ever expanding board game
+          collection
+        </p>
+        <div className="mt-8 w-full space-y-8 lg:mt-12 lg:space-y-14">
+          <CallToAction to="pick-a-game" color="primary">
+            Pick a game
+          </CallToAction>
+          <CallToAction to="library" color="secondary">
+            Browse library
+          </CallToAction>
         </div>
       </div>
     </div>
+  );
+}
+
+type CallToActionProps = {
+  to: string;
+  color: 'primary' | 'secondary';
+  children: React.ReactNode;
+};
+
+function CallToAction({ to, color, children }: CallToActionProps) {
+  return (
+    <Link
+      to={to}
+      className={clsx(
+        'flex w-full items-center justify-center rounded-md border border-transparent px-8 py-3 text-lg font-normal leading-8 focus:outline-none focus:ring-2 focus:ring-offset-2 lg:text-2xl lg:font-light lg:leading-10',
+        color === 'primary' &&
+          'bg-teal-700 text-slate-50 hover:bg-teal-800 focus:ring-teal-600',
+        color === 'secondary' &&
+          'bg-fuchsia-700 text-slate-50 hover:bg-fuchsia-800 focus:ring-fuchsia-600',
+      )}
+    >
+      {children}
+    </Link>
   );
 }
