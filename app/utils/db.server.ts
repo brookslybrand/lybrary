4aae793c578f9b19d00b9850d3876895cf159a79
brookslybrand <brookslybrand@gmail.com>
2023-07-15 10:10:00 -0500

    Setup prisma with knowingly wrong schema

---
+++ app/utils/db.server.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from '@prisma/client';
+import { singleton } from './singleton.server';
+
+const prisma = singleton('prisma', () => new PrismaClient());
+prisma.$connect();
+
+export { prisma };
---
+++ prisma/seed.ts
@@ -0,0 +1,33 @@
+import type { User } from '@prisma/client';
+import { prisma } from '~/utils/db.server';
+
+const user: User = {
+  id: 1,
+  email: 'brookslybrand@gmail.com',
+  name: 'Brooks',
+};
+
+async function seed() {
+  try {
+    await prisma.user.delete({
+      where: {
+        id: user.id,
+      },
+    });
+  } catch (error) {
+    console.log('No users to delete');
+  }
+
+  await prisma.user.create({
+    data: user,
+  });
+}
+
+seed()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
